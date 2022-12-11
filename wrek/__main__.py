36f690f566d4c0879ac0e04e4163420628f8d892
loyzbal <juanitobaldoaspler@gmail.com>
2022-12-11 18:38:14 +0800

    functions added

--- wrek/__main__.py
+++ wrek/__main__.py
@@ -8,14 +8,13 @@ from rich.table import Table
 console = Console()
 app = typer.Typer()
 
-@app.command(short_help = "Get all saved wifi passwords.")
-def getall():
+def getProfiles() -> list[str]:
+    profiles: list[str] = []
+
     meta_data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles'])
     data = meta_data.decode('utf-8', errors ="backslashreplace")
     data = data.split('\n')
 
-    profiles = []
-    
     for i in data:
         if "All User Profile" in i :
             i = i.split(":")
@@ -23,8 +22,24 @@ def getall():
             i = i[1:-1]
             profiles.append(i)
 
+    return profiles
+
+
+def getMoreInfo(SSID: str, keyInfo: str) -> str:
+    results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', SSID, 'key=clear'])
+    results = results.decode('utf-8', errors = "backslashreplace")
+    results = results.split('\n')
+    keyValue = [b.split(":")[1][1:-1] for b in results if keyInfo in b][0]
+
+    return keyValue
+
+
+@app.command(short_help = "Get all saved wifi passwords.")
+def getall():
+    profiles = getProfiles()
+
     table = Table(show_header = True, header_style = "blue", box = box.SQUARE_DOUBLE_HEAD)
-    table.add_column("Wi-Fi Name")
+    table.add_column("SSID Name")
     table.add_column("Security")
     table.add_column("Password")
     table.add_column("Authentication")
@@ -32,23 +47,15 @@ def getall():
     table.add_column("Cipher")
     table.add_column("Vendor")
 
-    for i in profiles:
-        try:
-            results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear'])
-            results = results.decode('utf-8', errors = "backslashreplace")
-            results = results.split('\n')
+    for profile in profiles:
+        security = getMoreInfo(profile, "Security key")
+        password = getMoreInfo(profile, "Key Content")
+        auth = getMoreInfo(profile, "Authentication")
+        dataLimit = getMoreInfo(profile, "Over Data Limit")
+        cipher = getMoreInfo(profile, "Cipher")
+        vendor = getMoreInfo(profile, "Vendor extension")
 
-            secKeys = [b.split(":")[1][1:-1] for b in results if "Security key" in b]
-            passwords = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
-            authentications = [b.split(":")[1][1:-1] for b in results if "Authentication" in b]
-            maxDataLimits = [b.split(":")[1][1:-1] for b in results if "Over Data Limit" in b]
-            vendors = [b.split(":")[1][1:-1] for b in results if "Vendor extension" in b]
-            ciphers = [b.split(":")[1][1:-1] for b in results if "Cipher" in b]
-
-            table.add_row(i, secKeys[0], passwords[0], authentications[0], maxDataLimits[0], ciphers[0], vendors[0])
-
-        except subprocess.CalledProcessError:
-            print("Encoding Error Occurred")
+        table.add_row(profile, security, password, auth, dataLimit, cipher, vendor)
 
     console.print(table)
 
